0c3f77e4e3cfc7a6a0a0c6cdfa8da29aa0ac360c
LeeBhin <bini4039@naver.com>
2024-03-23 01:50:11 +0900

    title, favicon, 검색 팝업의 리스트

--- src/components/SearchPopup.jsx
+++ src/components/SearchPopup.jsx
@@ -1,5 +1,6 @@
 import '../styles/searchPopup.css'
 import { HiOutlineSearch } from "react-icons/hi";
+import { RiShareBoxLine } from "react-icons/ri";
 
 function SearchPopup() {
     return (
@@ -8,6 +9,50 @@ function SearchPopup() {
                 <div className="searchWrap">
                     <HiOutlineSearch />
                     <input type="text" placeholder='여기에 입력하여 검색' />
+                    <div className="searchImg"></div>
+                </div>
+            </div>
+
+
+            <div className="searchInnerWrap">
+                <div className="shortCut">
+                    <span className='title'>
+                        작품
+                    </span>
+
+                    <div className="shortList">
+                        <div className="listWrap">
+                            <div className="listIcon"></div>
+                            <div className="listName">ChatHSF</div>
+                            <RiShareBoxLine />
+                        </div>
+                        <div className="listWrap">
+                            <div className="listIcon"></div>
+                            <div className="listName">Sebbung 커뮤니티</div>
+                            <RiShareBoxLine />
+                        </div>
+                        <div className="listWrap">
+                            <div className="listIcon"></div>
+                            <div className="listName">Windows 11</div>
+                            <RiShareBoxLine />
+                        </div>
+                        <div className="listWrap">
+                            <div className="listIcon"></div>
+                            <div className="listName">세명랜드</div>
+                            <RiShareBoxLine />
+                        </div>
+                        <div className="listWrap">
+                            <div className="listIcon"></div>
+                            <div className="listName">프로젝트</div>
+                            <RiShareBoxLine />
+                        </div>
+                    </div>
+                </div>
+
+                <div className="activity">
+                    <span className='title'>
+                        이빈에 대해 알아보기
+                    </span>
                 </div>
             </div>
         </div>
