b3357afdb98dff29b87c748432da53c47d036c09
LeeBhin <bini4039@naver.com>
2024-03-30 15:36:41 +0900

    시작/검색 팝업 리팩토링

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,45 +7,50 @@ import StartPopup from '../components/StartPopup';
 function Home() {
     const [isSearchOn, setIsSearchOn] = useState(false)
     const [isStartOn, setIsStartOn] = useState(false)
-
+    
     useEffect(() => {
-        // console.log('search', isSearchOn)
-        // console.log('start', isStartOn)
-        searchPopupMove(isSearchOn)
-        startPopupMove(isStartOn)
+        togglePopupMove('.searchPopup', isSearchOn, 'searchUp', 'searchDown')
+        togglePopupMove('.startPopupWrap', isStartOn, 'startUp', 'startDown')
     }, [isSearchOn, isStartOn]);
 
-    const changeSearch = isSearchOn => {
-        if (isStartOn) {
-            setIsStartOn(!isStartOn)
-        }
+    const changeSearch = (isSearchOn) => {
+        if (isStartOn) setIsStartOn(false)
         setIsSearchOn(isSearchOn)
+        togglePopupMove('.searchPopup', isSearchOn, 'searchUp', 'searchDown')
     }
 
-    const changeStart = isStartOn => {
-        if (isSearchOn) {
-            setIsSearchOn(!isSearchOn)
-        }
+    const changeStart = (isStartOn) => {
+        if (isSearchOn) setIsSearchOn(false)
         setIsStartOn(isStartOn)
+        togglePopupMove('.startPopupWrap', isStartOn, 'startUp', 'startDown')
     }
+
 
     const searchPopupMove = (isSearchOn) => {
-        const searchElement = document.querySelector('.searchPopup')
-        if (isSearchOn) {
-            searchElement.className = "searchPopup searchUp"
-        } else {
-            searchElement.className = "searchPopup searchDown"
-        }
+        togglePopupMove('.searchPopup', isSearchOn, 'searchUp', 'searchDown')
     }
 
     const startPopupMove = (isStartOn) => {
-        const searchElement = document.querySelector('.startPopupWrap')
-        if (isStartOn) {
-            searchElement.className = "startPopupWrap startUp"
+        togglePopupMove('.startPopupWrap', isStartOn, 'startUp', 'startDown')
+    }
+
+    const togglePopupMove = (element, isOn, classUp, classDown) => {
+        const popupElement = document.querySelector(element)
+        if (isOn) {
+            popupElement.style.transition = '.2s ease'
+            popupElement.classList.remove(classDown)
+            popupElement.classList.add(classUp)
         } else {
-            searchElement.className = "startPopupWrap startDown"
+            if (!isSearchOn && !isStartOn) {
+                popupElement.style.transition = '.2s cubic-bezier(0.88, 0, 0.88, 1)'
+            } else {
+                popupElement.style.transition = '0s'
+            }
+            popupElement.classList.remove(classUp)
+            popupElement.classList.add(classDown)
         }
     }
+
 
     return (
         <div className="home">
