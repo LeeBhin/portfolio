cb5731b830d8c51f19e85b2babcb0c95d08896b7
LeeBhin <bini4039@naver.com>
2024-04-23 00:30:00 +0900

    폴더 경로, 검색, 툴바

--- src/components/Folder.jsx
+++ src/components/Folder.jsx
@@ -1,6 +1,22 @@
 import '../styles/folder.css'
 import { Images } from '../images/Images';
 import { CgClose } from "react-icons/cg";
+import { PiPlus, PiArrowLeftLight, PiArrowRightLight, PiArrowUpLight, PiMinus } from "react-icons/pi";
+import { GoHome } from "react-icons/go";
+import { RxReload } from "react-icons/rx";
+import { RiArrowRightSLine } from "react-icons/ri";
+import { IoIosSearch } from "react-icons/io";
+import { IoSquareOutline, IoCloseOutline } from "react-icons/io5";
+import { GrClose } from "react-icons/gr";
+import { TbSquare } from "react-icons/tb";
+import { CiCirclePlus } from "react-icons/ci";
+import { GrFormDown } from "react-icons/gr";
+import { TbCut } from "react-icons/tb";
+import { FaRegCopy } from "react-icons/fa6";
+import { BiRename } from "react-icons/bi";
+import { FaRegShareSquare } from "react-icons/fa";
+import { GoTrash } from "react-icons/go";
+import { LiaClipboardSolid } from "react-icons/lia";
 
 function Folder({ }) {
     return (
@@ -18,25 +34,59 @@ function Folder({ }) {
 
                             <div className="tabClose"><CgClose /></div>
                         </div>
-                        <div className="tabPlus">+</div>
+                        <div className="tabPlus"><PiPlus /></div>
                     </div>
                 </div>
 
                 <div className="minMaxWrap">
-                    <div className="minBtn">ㅡ</div>
-                    <div className="maxBtn">ㅁ</div>
-                    <div className="closeBtn">X</div>
+                    <div className="minBtn"><PiMinus size={"16px"} /></div>
+                    <div className="maxBtn"><TbSquare size={"14px"} /></div>
+                    <div className="closeBtn"><GrClose size={"13px"} /></div>
                 </div>
             </div>
             <div className="midHeader">
                 <div className="moveWrap">
-                    <div className="prev">→</div>
-                    <div className="next">←</div>
-                    <div className="up">↑</div>
-                    <div className="reload">○</div>
+                    <div className="prev"><PiArrowLeftLight /></div>
+                    <div className="next">< PiArrowRightLight /></div>
+                    <div className="up"><PiArrowUpLight /></div>
+                    <div className="reload"><RxReload size={"14px"} /></div>
+                </div>
+
+                <div className="directory">
+                    <div className="icon"><GoHome size={"17px"} /></div>
+                    <div className="arrow" id='btn'><RiArrowRightSLine /></div>
+                    <div className="txt" id='btn'>홈</div>
+                </div>
+
+                <div className="search">
+                    <div className="searchWrap">
+                        <input type="search" placeholder='홈 검색' />
+                        <div className="searchIcon"><IoIosSearch /></div>
+                    </div>
                 </div>
             </div>
-        </div>
+
+            <div className="botHeader">
+                <div className="newWrap">
+                    <div className="newBtn"><CiCirclePlus /></div>
+                    <div className="newTxt">새로 만들기</div>
+                    <div className="newDrop"><GrFormDown size={"12px"} /></div>
+                </div>
+
+                <div className="toolWrap">
+                    <div className="cut"><TbCut size={"20px"} /></div>
+                    <div className="copy"><FaRegCopy size={"20px"} /></div>
+                    <div className="clip"><LiaClipboardSolid size={"20px"} /></div>
+                    <div className="rename"><BiRename size={"20px"} /></div>
+                    <div className="share"><FaRegShareSquare size={"20px"} /></div>
+                    <div className="delete"><GoTrash size={"20px"} /></div>
+                </div>
+            </div>
+
+            <div className="leftHeader">
+
+            </div>
+        </div >
     );
 }
 
