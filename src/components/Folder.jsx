5e125788fd43dee7e6499220a0764d4f44892cd6
LeeBhin <bini4039@naver.com>
2024-04-23 23:32:56 +0900

    폴더 topHead, midHead 디테일, hover

--- src/components/Folder.jsx
+++ src/components/Folder.jsx
@@ -25,6 +25,9 @@ function Folder({ }) {
                 <div className="topHeader">
                     <div className="tabWrap">
                         <div className="folderTab">
+                            <svg id="folderTab" viewBox="0 0 250 31" shape-rendering="geometricPrecision" text-rendering="geometricPrecision">
+                                <path d="M34.806874,71.257641q3.219742-.482265,4.666691-2.973539.451881-1.42284,0-18.48985q0-6.93695,6.434401-9.522052.911221,0,227.804183,0q7.285117,1.768101,7.285117,9.522052c0,.000001,0,12.324742,0,17.335137c0,3.137052,3.668328,4.128252,3.668328,4.128252q0,0-249.85872,0Z" transform="translate(-34.740162-40.2722)" fill="#f0f5f9" stroke-width="0.682" />
+                            </svg>
                             <div className="iconWrap">
                                 <div className="tabIcon">
                                     <img src={Images.HOME} alt="" />
@@ -39,9 +42,9 @@ function Folder({ }) {
                 </div>
 
                 <div className="minMaxWrap">
-                    <div className="minBtn"><PiMinus size={"16px"} /></div>
-                    <div className="maxBtn"><TbSquare size={"14px"} /></div>
-                    <div className="closeBtn"><GrClose size={"13px"} /></div>
+                    <div className="minBtn"><PiMinus size={"14px"} /></div>
+                    <div className="maxBtn"><TbSquare size={"12px"} /></div>
+                    <div className="closeBtn"><GrClose size={"12px"} /></div>
                 </div>
             </div>
             <div className="midHeader">
@@ -84,6 +87,7 @@ function Folder({ }) {
             </div>
 
             <div className="leftHeader">
+
 
             </div>
         </div >
